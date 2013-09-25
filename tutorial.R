3b6ec5943479aa968ff5cb81f6ffe9cfe975a9e9
Adamstrd <theresaadams@man-97-107.reshall.berkeley.edu>
2013-09-25 12:15:02 -0700

    bar chart

--- tutorial.R
+++ tutorial.R
@@ -1,1 +1,18 @@
+"I should exercise more."
 
+
+1+1
+
+
+1==1
+
+
+animals <- c ("bears", "monkeys", "donkeys")
+
+class (prices)
+prices$X2013[1:10]
+
+plot(prices$X2013)
+
+
+barplot(prices$X2013)
